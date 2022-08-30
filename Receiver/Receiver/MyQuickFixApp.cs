e777f1bbc532d45f98817f0cc0742807bf0b7c8c
Navnit Anuth <navz@users.noreply.github.com>
2022-08-30 18:11:58 +0100

    Receiver accepted

---
+++ Receiver/Receiver/MyQuickFixApp.cs
@@ -0,0 +1,59 @@
+﻿using QuickFix;
+using QuickFix.Fields;
+
+namespace Receiver
+{
+    public class MyQuickFixApp : MessageCracker, IApplication
+    {
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            // ToAdmin - all outbound admin level messages pass through this callback.
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            // FromAdmin - every inbound admin level message will pass through this method,
+            // such as heartbeats, logons, and logouts.
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        {
+            // ToApp - all outbound application level messages pass through this
+            // callback before they are sent. If a tag needs to be added to every outgoing message,
+            // this is a good place to do that.
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            //FromApp - every inbound application level message will pass through this method,
+            //such as orders, executions, security definitions, and market data.
+            Crack(message, sessionID);
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            // OnCreate - this method is called whenever a new session is created.
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            // OnLogout - notifies when a session is offline - either from an exchange of logout messages
+            // or network connectivity loss.
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            // OnLogon - notifies when a successful logon has completed.
+        }
+
+        public void OnMessage(QuickFix.FIX44.NewOrderSingle ord, SessionID sessionID)
+        {
+            ProcessOrder(ord.Price, ord.OrderQty, ord.Account);
+        }
+
+        protected void ProcessOrder(Price price, OrderQty quantity, Account account)
+        {
+            //...
+        }
+    }
+}
--- Receiver/Receiver/Program.cs
+++ Receiver/Receiver/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using Acceptor;
+using QuickFix;
 
 namespace Receiver
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const string HttpServerPrefix = "http://127.0.0.1:5080/";
+
+        [STAThread]
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initiator is the FIX term for client -
+            // we use an Initiator when we are connecting to another party.
+
+            // Acceptor is the FIX term for server -
+            // we use an Acceptor when other parties are connecting to us.
+
+            var settings = new SessionSettings("config\\sample_acceptor.cfg");
+            IApplication myApp = new MyQuickFixApp();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            var srv = new HttpServer(HttpServerPrefix, settings);
+            
+            // Acceptor - switch to SocketInitiator for Initiator
+            var acceptor = new ThreadedSocketAcceptor(
+                myApp,
+                storeFactory,
+                settings,
+                logFactory);
+
+            acceptor.Start();
+            srv.Start();
+
+            Console.WriteLine("View Executor status: " + HttpServerPrefix);
+            Console.WriteLine("press <enter> to quit");
+            Console.Read();
+
+            srv.Stop();
+            acceptor.Stop();
         }
     }
 }
